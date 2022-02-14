54e8fb1b160ec2c190c0b98cfe001b7e4e7b7cec
Alice Sousa <77370190+Alicesinha@users.noreply.github.com>
2022-02-14 12:28:17 -0300

    Add files via upload

---
+++ reduce/challenge.js
@@ -0,0 +1,22 @@
+/*
+ 4 parametros aceitos:
+-acumulador
+-valor atual
+-index
+-array completo
+*/
+
+
+const companies = [
+    {name: "Samsung", marketValue: 50, CEO: "Kim Hyun Suk", foundedOn: 1938},
+    {name: "Microsoft", marketValue: 415, CEO: "Satya Nodella", foundedOn: 1975},
+    {name: "Intel", marketValue: 117, CEO: "Brian Krzanich", foundedOn: 1968},
+    {name: "Facebook", marketValue: 383, CEO: "Mark Zuckenberg", foundedOn: 2004},
+    {name: "Spotify", marketValue: 30, CEO: "Daniel Ek", foundedOn: 2006},
+    {name: "Apple", marketValue: 845, CEO: "Tim Cook", foundedOn: 1976},
+];
+
+const sumMarketValue = companies.reduce ((acc, current) =>{ 
+    return acc + current.marketValue;
+},0)
+console.log(sumMarketValue)
